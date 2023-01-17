dec74eae1761a3f9cd6706b15597dfe5dbb03118
Nelson5501 <nelsoncaterino@gmail.com>
2023-01-17 01:42:20 +0100

    suppression Suppression du carousel + ajout du logo + changement de navbar pour navbar avec dropdown + ajout de la page Photos (pas encore fonctionnel) et de la page Présentation (pas encore fonctionnel)

--- marni/views.py
+++ marni/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from .models import Post
 
+def base_viws(request):
+    return render(request, "base.html")
 
 class HomePageView(TemplateView) :  
     template_name = 'home.html'
--- theatre/settings.py
+++ theatre/settings.py
@@ -56,9 +56,7 @@ ROOT_URLCONF = 'theatre.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            '/home.html'
-            ],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
